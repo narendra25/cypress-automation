862560df2e4b5835c0de4bb395d1625c411c7092
narendra25 <anrsmile96@gmail.com>
2023-10-16 16:08:44 +0530

    Xpath Locators

---
+++ cypress/e2e/XpathLocators.cy.js
@@ -0,0 +1,22 @@
+//Xpath
+//--------------------------------------------------
+//npm install -D cypress-xpath
+
+//Add entry in commands.js 
+////<reference types="cypress-xpath"/>
+
+//e2e.js
+
+//require('cypress-xpath')
+
+describe('Xpath Locators',()=>{
+    it('xpath',()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        //xpaths
+        cy.xpath("//input[@name='username']").type('Admin')
+        cy.xpath("//input[@name='password']").type('admin123')
+        cy.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']").click()
+
+    })
+})
